e5cdf19cc2dbf06c2233fc03e236f126ed5268cd
Namnika <janbandhunamnika@gmail.com>
2025-05-22 14:39:23 +0530

    add Todo Summary Test

---
+++ TodoBackend/src/test/java/com/todoSummaryApplication/todoSummary/TodoSummaryTests.java
@@ -0,0 +1,13 @@
+package com.todoSummaryApplication.todoSummary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoSummaryTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
