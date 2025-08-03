c5ed02220e50ff6b4815beea068ceac47fdc3047
Namnika <janbandhunamnika@gmail.com>
2025-08-03 14:22:11 +0530

    add env to urls from config

--- TodoBackend/src/main/java/com/todoSummaryApplication/todoSummary/config/CorsConfig.java
+++ TodoBackend/src/main/java/com/todoSummaryApplication/todoSummary/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.todoSummaryApplication.todoSummary.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,13 +9,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
+    @Value("${frontend.url}")
+    private String frontendUrl;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // allowed all paths
-                        .allowedOrigins("http://localhost:3000") // allowed React App origin
+                        .allowedOrigins(frontendUrl) // allowed React App origin
                         .allowedMethods("GET", "POST", "PUT", "DELETE") // allowed all methods
                         .allowedHeaders("*"); // allowed all headers
 
